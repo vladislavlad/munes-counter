c1d07a9681d9736a8765977b7a5c5615be147538
Vladislav Kartashov <kartashovladislav@gmail.com>
2023-10-09 15:06:58 +0300

    Test fix

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Start your work Button', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Start your work/i);
   expect(linkElement).toBeInTheDocument();
